8292ca0a74d8279b7fc39c7751d3406ff0d4120b
Padrigon, Lynbert <123610073+Skilt@users.noreply.github.com>
2023-07-28 20:26:20 +0800

    + Regex: Matching and Exracting Data (28)

---
+++ test.py
@@ -0,0 +1,7 @@
+story = """
+In a hidden forest, a courageous young sorceress stumbled upon a mystical amulet.
+As she wore it, whispers of ancient magic consumed her, granting incredible powers. 
+But with great power came a daunting choice: use it for good or succumb to darkness.
+Her destiny began, teetering on a delicate thread.
+"""
+
