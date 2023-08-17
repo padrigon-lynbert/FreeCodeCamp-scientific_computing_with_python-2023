dbeff430764b7eef7571e6682248bbfad0b47a9f
Padrigon, Lynbert <123610073+Skilt@users.noreply.github.com>
2023-08-17 18:47:00 +0800

    37. Web Services: XML.

--- test.file/test.py
+++ test.file/test.py
@@ -1,4 +1,4 @@
-import re
+# import re 
 import random
 
 words = story = """
@@ -6,12 +6,12 @@ In a hidden forest, a courageous young sorceress stumbled upon a mystical amulet
 As she wore it, whispers of ancient magic consumed her, granting incredible powers. 
 But with great power came a daunting choice: use it for good or succumb to darkness.
 Her destiny began, teetering on a delicate thread.
-""".lower().split()
+""".lower().split() #lowercase story is now inside words=[] put as word 
 
 words = [word.strip('.,:') for word in words] #remove special chars
-exclude_words = ["do", "a", "in", "as", "on", "it", "or", "to"] #specify what to exclude
+exclude_words = ["do", "a", "in", "as", "on", "it", "or", "to", "of", "for", "with", "she", "her", "but"] #specify what to exclude
 words = [word for word in words if word not in exclude_words] #remove excluded
 
-for i in range(10): #now test
+for i in range(10): #test(10 times) if working
     random_word = random.choice(words)
     print(random_word)
