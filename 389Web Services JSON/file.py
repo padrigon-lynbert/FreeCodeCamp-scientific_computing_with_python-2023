c8e663fc630fe64b52363c9e956f1a275ad1109d
Padrigon, Lynbert <123610073+Skilt@users.noreply.github.com>
2023-08-20 14:12:23 +0800

    39-done.

---
+++ 389Web Services JSON/file.py
@@ -0,0 +1,17 @@
+import json
+data = '''
+  [
+    { "id" : "001",
+      "x" : "2",
+     "name" : "Quincy"
+    } ,
+    { "id" : "009",
+      "x" : "7",
+      "name" : "Mrugesh"
+    }
+  ]
+'''
+info = json.loads(data)
+print(info[1]['name'])
+
+# Mrugesh
