5c1f2507ce5d39582a65a1e6e8d1745a59d3a2c9
Padrigon, Lynbert <123610073+Skilt@users.noreply.github.com>
2023-09-02 16:13:02 +0800

    comment

--- 13 Loop Idioms/file.py
+++ 13 Loop Idioms/file.py
@@ -6,3 +6,6 @@ for itervar in [3, 41, 12, 9, 74, 15]:
         break #error is line 6, it break the loop before iterating all values
     print("Loop:", itervar, smallest)
 print("Smallest:", smallest)
+
+# Before: None
+# Smallest: 3
