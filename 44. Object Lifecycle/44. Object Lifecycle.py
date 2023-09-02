0a6660d2654f751698c48ddbe1c01f168b1ea0a4
Padrigon, Lynbert <123610073+Skilt@users.noreply.github.com>
2023-09-02 18:00:22 +0800

    + 44

---
+++ 44. Object Lifecycle/44. Object Lifecycle.py
@@ -0,0 +1,22 @@
+class PartyAnimal:
+    x = 0
+    name = ''
+    def __init__(self, nam):
+        self.name = nam
+        print(self.name,'constructed')
+    def party(self):
+        self.x = self.x + 1
+        print(self.name,'party count',self.x)
+
+q = PartyAnimal('Quincy')
+m = PartyAnimal('Miya')
+
+q.party()
+m.party()
+q.party()
+
+# Quincy constructed
+# Miya constructed
+# Quincy party count 1
+# Miya party count 1
+# Quincy party count 2
